4de4e8b4ee33d6faf566abcf4732408975675de5
wind <wind@comtv.ru>
2017-05-22 01:30:57 +0300

    no commit message

--- src/main/java/com/github/winchopper/pd/Main.java
+++ src/main/java/com/github/winchopper/pd/Main.java
@@ -5,7 +5,6 @@ import com.github.windchopper.common.util.Pipeliner;
 import javafx.application.Application;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
@@ -18,7 +17,7 @@ public class Main extends Application {
     @Override public void start(Stage primaryStage) throws Exception {
         Rectangle2D primaryScreenVisualBounds = Screen.getPrimary().getVisualBounds();
 
-        primaryStage.setScene(Pipeliner.of(() -> new Scene(ContainerFactory.createSceneRoot(BorderPane::new), primaryScreenVisualBounds.getWidth() / 6, primaryScreenVisualBounds.getHeight() / 3))
+        primaryStage.setScene(Pipeliner.of(() -> new Scene(ContainerFactory.createSceneRoot(ContainerFactory::createPasswordTree), primaryScreenVisualBounds.getWidth() / 6, primaryScreenVisualBounds.getHeight() / 3))
             .set(scene -> scene::setFill, Color.TRANSPARENT)
             .add(scene -> scene::getStylesheets, singleton("styles.css"))
             .get());
--- src/main/java/com/github/winchopper/pd/ui/ContainerFactory.java
+++ src/main/java/com/github/winchopper/pd/ui/ContainerFactory.java
@@ -6,6 +6,8 @@ import javafx.geometry.Bounds;
 import javafx.geometry.Insets;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.BorderPane;
@@ -18,6 +20,7 @@ import name.wind.common.fx.behavior.WindowMoveResizeBehavior;
 import java.util.Random;
 import java.util.function.Supplier;
 
+import static java.util.Arrays.asList;
 import static java.util.Collections.singleton;
 
 public class ContainerFactory {
@@ -34,7 +37,7 @@ public class ContainerFactory {
 
     public static Parent createSceneRoot(Supplier<Region> containerFactory) {
         Random random = new Random();
-        Rotate rotate = new Rotate(0.1 * random.nextInt(30) *
+        Rotate rotate = new Rotate(0.1 * random.nextInt(25) *
             (random.nextBoolean() ? +1 : -1));
 
         Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
@@ -43,9 +46,14 @@ public class ContainerFactory {
         Bounds rotatedBounds = rotate.transform(initialBounds);
 
         return Pipeliner.of(BorderPane::new)
-            .add(outer -> outer::getStyleClass, singleton("outer-container"))
+            .add(outer -> outer::getStyleClass, asList("any", "outer-container"))
+            .set(outer -> outer::setBackground, new Background(
+                new BackgroundFill(
+                    Color.TRANSPARENT,
+                    null,
+                    null)))
             .set(outer -> outer::setCenter, Pipeliner.of(containerFactory)
-                .add(inner -> inner::getStyleClass, singleton("inner-container"))
+                .add(inner -> inner::getStyleClass, asList("any", "inner-container"))
                 .accept(inner -> new WindowMoveResizeBehavior().apply(inner))
                 .add(inner -> inner::getTransforms, singleton(rotate))
                 .accept(inner -> BorderPane.setMargin(inner, new Insets(Math.max(rotatedBounds.getWidth() - initialBounds.getWidth(),
@@ -55,6 +63,26 @@ public class ContainerFactory {
                         backgroundColors[random.nextInt(backgroundColors.length)],
                         null,
                         null)))
+                .get())
+            .get();
+    }
+
+    public static Region createPasswordTree() {
+        return Pipeliner.of(BorderPane::new)
+            .set(pane -> pane::setPadding, new Insets(20, 10, 20, 10))
+            .add(pane -> pane::getStyleClass, asList("any"))
+            .set(pane -> pane::setCenter, Pipeliner.of(TreeView<String>::new)
+                .accept(tree -> {
+                    TreeItem<String> word = new TreeItem<>("word");
+
+                    TreeItem<String> page = new TreeItem<>("page");
+                    page.getChildren().add(word);
+
+                    TreeItem<String> root = new TreeItem<>("root");
+                    root.getChildren().add(page);
+
+                    tree.setRoot(root);
+                })
                 .get())
             .get();
     }
