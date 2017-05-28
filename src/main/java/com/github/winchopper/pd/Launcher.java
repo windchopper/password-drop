652eb3c38dab33da50b8737d040d817006833f4a
wind <wind@comtv.ru>
2017-05-29 02:53:42 +0300

    no commit message

--- src/main/java/com/github/winchopper/pd/Launcher.java
+++ src/main/java/com/github/winchopper/pd/Launcher.java
@@ -5,12 +5,16 @@ import com.github.windchopper.common.util.Pipeliner;
 import javafx.application.Application;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
+import java.util.Collections;
 import java.util.ResourceBundle;
+
+import static java.util.Collections.singleton;
 
 public class Launcher extends Application {
 
@@ -19,12 +23,13 @@ public class Launcher extends Application {
     @Override public void start(Stage primaryStage) throws Exception {
         Rectangle2D primaryScreenVisualBounds = Screen.getPrimary().getVisualBounds();
 
-        primaryStage.setScene(Pipeliner.of(() -> new Scene(ContainerFactory.createSceneRoot(), primaryScreenVisualBounds.getWidth() / 6, primaryScreenVisualBounds.getHeight() / 3))
-//            .set(scene -> scene::setFill, Color.TRANSPARENT)
+        primaryStage.setScene(Pipeliner.of(() -> new Scene(ContainerFactory.createSceneRoot(ContainerFactory::createPasswordTree), primaryScreenVisualBounds.getWidth() / 6, primaryScreenVisualBounds.getHeight() / 3))
+            .set(scene -> scene::setFill, Color.TRANSPARENT)
+            .add(scene -> scene::getStylesheets, singleton("/com/github/windchopper/pd/styles.css"))
             .get());
 
         primaryStage.setTitle(bundle.getString("window.main.title"));
-        primaryStage.initStyle(StageStyle.UTILITY);
+        primaryStage.initStyle(StageStyle.TRANSPARENT);
         primaryStage.show();
     }
 
